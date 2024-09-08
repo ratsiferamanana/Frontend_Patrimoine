06cc334b499c42b82a197eecc5725d1059a405b5
TSIFERANA <hei.jimmy.5@gmail.com>
2024-09-08 22:08:04 +0300

    fix: rename url

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,13 +1,13 @@
 // frontend/src/ApiService.js
 export async function fetchPatrimoine() {
-  const response = await fetch('https://backend-patrimoine-2.onrender.com');
+  const response = await fetch('https://backend-patrimoine-2.onrender.com/api/patrimoine');
   const data = await response.json();
   return data;
 }
 
   
   export async function updatePatrimoine(newData) {
-    const response = await fetch('https://backend-patrimoine-2.onrender.com', {
+    const response = await fetch('https://backend-patrimoine-2.onrender.com/api/patrimoine', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
